0492712a9d9cd0c64e7596c152a070639621da6e
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-12-06 13:52:36 +0100

    fix tailwind errors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { store } from "./store/taskStore";
 import App from "./App.vue";
-import "./assets/tailwind.css";
+import './assets/tailwind.css'
 
 createApp(App).use(store).mount("#app");
